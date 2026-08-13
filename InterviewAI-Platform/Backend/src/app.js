bb8d9388ec3b144d8ef2c49cfe6af211b54d9080
Puja Barman <puja84879@gmail.com>
2026-08-14 00:41:34 +0530

    Update frontend deployment configuration

--- InterviewAI-Platform/Backend/src/app.js
+++ InterviewAI-Platform/Backend/src/app.js
@@ -7,10 +7,12 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "https://interviewai-platform-1.onrender.com"
+    ],
     credentials: true
-}))
-
+}));
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
